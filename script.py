d91ebe005796482114f875fc4804fad2ecda39f2
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-10-03 00:44:55 +0300

    change

--- script.py
+++ script.py
@@ -12,7 +12,6 @@ LOGIN = '+905372653209'
 PASSWORD = 'can1202'
 BASE_DIR = Path(__file__).resolve().parent
 
-
 def get_api():
     vk_session = vk_api.VkApi(LOGIN, PASSWORD)
     vk_session.auth()
@@ -26,19 +25,25 @@ def get_users_links(file_path: str):
     return user_link_data
 
 
-def save_photo(username: str, image_url: str):
-    image_name = re.search('.+/(.+)\?', image_url)
+def save_photo(username: str, image_url: str, path_dir_images:str):
+    image_name_search = re.search('.+/(.+\.jpg)\?', image_url)
+    if image_name_search:
+        image_name = image_name_search.group(1)
+    else:
+        return
     h = httplib2.Http('.cache')
     response, content = h.request(image_url)
-    path = os.path.join(BASE_DIR, username, image_name.group(1))
-    dir_images = os.path.join(BASE_DIR, username)
+    if not os.path.exists(path_dir_images):
+        os.mkdir(path_dir_images)
+    path = os.path.join(path_dir_images, username, image_name)
+    dir_images = os.path.join(path_dir_images, username)
     if not os.path.exists(dir_images):
         os.mkdir(dir_images)
     with open(path, 'wb') as out:
         out.write(content)
 
 
-def main(file_links: str):
+def main(file_links: str, path_dir_images: str):
     vk = get_api()
     users_links = get_users_links(file_links)
     for user_link in users_links:
@@ -55,12 +60,13 @@ def main(file_links: str):
             if photos['items']:
                 for item in photos['items']:
                     image_url = item['sizes'][-1]['url']
-                    save_photo(username, image_url)
+                    save_photo(username, image_url, path_dir_images)
                     time.sleep(DELAY)
         time.sleep(DELAY)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3 and sys.argv[2] == 'download':
-        path = os.path.join(BASE_DIR, sys.argv[1])
-        main(path)
+    if len(sys.argv) == 3:
+        path_list_urls = os.path.join(BASE_DIR, sys.argv[1])
+        path_dir_images = os.path.join(BASE_DIR, sys.argv[2])
+        main(path_list_urls, path_dir_images)
